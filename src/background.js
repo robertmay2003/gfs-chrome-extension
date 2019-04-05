8befda4e4fca33fdef3159fac772a80b7fbff15a
robertmay2003 <robertmay2003@gmail.com>
2019-04-05 11:56:55 -0400

    Begun minor background work

--- src/background.js
+++ src/background.js
@@ -1,1 +1,11 @@
 // Background stuff here
+
+chrome.runtime.onInstalled.addListener(function() {
+
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId , info) {
+	if (info.status === 'complete') {
+		console.log(`Gruh: ${tabId}`);
+	}
+});
