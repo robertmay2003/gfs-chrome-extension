c52e574e70e18fbdd28370f4bcf63f1558f3c89c
Jekeun Jung <jekeunjung@Jekeuns-MacBook-Pro.local>
2019-04-12 14:25:59 -0400

    date with id

--- src/background.js
+++ src/background.js
@@ -3,9 +3,18 @@
 chrome.runtime.onInstalled.addListener(function() {
 
 });
-
+let global{
+	count:0,
+}
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
 	if (info.status === 'complete') {
+		global.count = global.count + 1;
+		if (global.count === 2) {
+			let d{
+				date()
+			}
+		}
+
 
 	}
 });
