f07da326f600fb18a616e2fd2e7dfe71946a5a6f
robertmay2003 <robertmay2003@gmail.com>
2019-04-12 14:14:17 -0400

    Things

--- src/background.js
+++ src/background.js
@@ -6,6 +6,6 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
 	if (info.status === 'complete') {
-		console.log(`Gruh: ${tabId}`);
+
 	}
 });
