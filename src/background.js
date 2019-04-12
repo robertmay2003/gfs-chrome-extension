638189a70f507a54d1e64c4e34eb36b190e78e3d
robertmay2003 <robertmay2003@gmail.com>
2019-04-12 14:24:15 -0400

    Use chrome local storage

--- src/background.js
+++ src/background.js
@@ -1,7 +1,14 @@
 // Background stuff here
+function updateTabData(data, callback) {
+	chrome.storage.local.set({tabData: data}, callback());
+}
+
+function getTabData(data, callback) {
+	chrome.storage.local.get(['tabData'], callback());
+}
 
 chrome.runtime.onInstalled.addListener(function() {
-
+	
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
