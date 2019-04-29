12757167b59fdf5da6677ae258940a60450eb6f7
Dylan Marchlinski <dylanmarchlinski@Dylans-MacBook-Air.local>
2019-04-29 08:21:28 -0400

    added rectangle div

--- src/content.js
+++ src/content.js
@@ -1,6 +1,7 @@
 window.requestAnimationFrame(() => {
   var div = document.createElement("div");
   div.id = "gfsInformationDiv";
+  div.innerHTML = "Hello";
   document.body.appendChild(div);
 	// Run this code in the actual page
 	// can be run manually by popup.js or automatically on page load through manifest.json
